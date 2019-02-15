1b00ee5673509d125c33e23113a2a80519c5861f
nicodarge <nicolas.dargenlieu@epfl.ch>
2019-02-15 23:30:33 +0100

    Implemented delete account

--- Vues/Parameters/Parameters.js
+++ Vues/Parameters/Parameters.js
@@ -1,8 +1,11 @@
 // Imports
 import React from 'react'
-import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import { View, Text, ActivityIndicator, StyleSheet, Button } from 'react-native'
 import firebase from "firebase"
-import firebaseApp from '../../firebase';
+import firebaseApp from '../../firebase'
+import Dialog from 'react-native-dialog'
+import ReAuthDialog from '../Dialogs/ReAuthentication'
+import BasicDialog from '../Dialogs/BasicOkCancel'
 
 export default class Parameters extends React.Component {
 
@@ -14,16 +17,94 @@ export default class Parameters extends React.Component {
 	  this.state = {
 			user: user? user : null,
 			name: user? user.displayName : null,
-			email: user? user.email : ''
+			email: user? user.email : '',
+			errorMessage : '',
+			deleteDialogVisibility : false,
+			deleteDialogErrorMessage : '',
+			reAuthDialVis : false
 		};
+
 	}
 
+
+	// Handles signout logic
+	signOut = () => {
+		firebase.auth().signOut().then(
+			this.props.navigation.navigate('Loading')
+		).catch(
+			error => this.setState({errorMessage : error.message})
+		)
+	}
+
+	//Switch to confirmation fragment
+	confirmDelete = () => {
+		this.setState({ deleteDialogVisibility : false });
+		this.setState({ reAuthDialVis : true });
+	}
+
+	// Delete account
+	deleteAccount = () => {
+
+		firebase.auth().currentUser.delete().then(() => {
+			this.props.navigation.navigate('Loading');
+		}).catch( error => {
+			this.setState({ deleteDialogErrorMessage : error.message });
+		})
+
+	}
+
+	// Hides delete dialog
+	hideDialog = () => {
+		this.setState({ deleteDialogVisibility: false})
+	}
+
+	// Shows delete Dialog
+	showDialog = () => {
+		this.setState({ deleteDialogVisibility : true })
+	}
+
+	// Renders the visual
 	render() {
 		return (
 			<View style={styles.container}>
+
+				<BasicDialog
+					visible={ this.state.deleteDialogVisibility }
+					title='Warning'
+					description='Do you really want to delete your account. This cannot be undone.'
+					okFunc = {this.confirmDelete}
+					cancelFunc = {this.hideDialog}
+				/>
+
+				<ReAuthDialog
+					onCancel={ ()=>{
+						this.setState({reAuthDialVis : false});
+						this.props.navigation.navigate('Loading');
+					} }
+					onOk={this.deleteAccount}
+					visible={ this.state.reAuthDialVis}
+				/>
+
+
 				<Text>
 					Hi {this.state.name ? this.state.name : this.state.email} !
 				</Text>
+
+				<Text style={{color: 'red'}}>
+					{this.state.errorMessage}
+				</Text>
+
+				<Button
+					title='Sign out'
+					onPress={this.signOut}
+				/>
+
+				<Button
+					title='Delete Account'
+					color='red'
+					onPress={ this.showDialog }
+				/>
+
 			</View>
 		)
 	}
